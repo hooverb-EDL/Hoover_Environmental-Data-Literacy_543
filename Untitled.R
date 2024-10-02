cf10cc585ac2d04b241c2e72f07f12ed8856811f
git <rebeccahoover@Rebeccas-MacBook-Air.local>
2024-10-01 21:58:44 -0400

    prac

---
+++ Untitled.R
@@ -0,0 +1,18 @@
+foo <- function ( x , y )
+  
+  { z <- x* y
+  
+return ( z )
+  
+}
+
+x<- 10
+y <- 20
+
+myans <- foo (12, 45454)
+
+bar <- function() {return(rnorm(100))}
+
+myHist <- function(x) {hist(x)}
+
+foo <- functu                            8
